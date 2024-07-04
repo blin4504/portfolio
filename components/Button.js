1d211953a69d5b1f539f76a4312f41ae53748801
blin4504 <bl4504@g.rit.edu>
2024-07-04 13:54:26 -0400

    chore: fix up buttons

--- components/Button.js
+++ components/Button.js
@@ -7,7 +7,8 @@ export default function Button({ href, label, newTab, children}) {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="inline-flex font-bold items-center mt-4 px-3 py-2 border-4 border-solid border-color_text shadow-custom_shadow shadow-color_text hover:bg-color_text hover:text-color_background text-sm sm:text-base"
+      className="inline-flex font-bold items-center justify-center mt-4 px-3 py-2 border-4 border-solid border-color_text shadow-custom_shadow shadow-color_text hover:bg-color_text hover:text-color_background text-sm sm:text-base"
+      style={{ minWidth: "150px", minHeight: "40px" }} 
     >
       {children}
       <span className="ml-2">{label}</span>
