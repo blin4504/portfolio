5589708c0c4b81a4ec7d55a2c5c06b2c2ed47e66
blin4504 <bl4504@g.rit.edu>
2024-08-17 20:52:34 -0400

    chore: update website info

--- app/data/Projects.js
+++ app/data/Projects.js
@@ -17,6 +17,12 @@ const projects = [
     description: "A fruit e-store web application built with a group of four to learn software engineering practices.",
     techs: ["Angular", "Java", "Typescript", "Spring Boot", "JUnit"]
   },
+  {
+    title: "Meddy",
+    link: "https://github.com/blin4504/meddy",
+    description: "Project for Google AI Competition. Meddy is an AI-powered medical assistant app designed to enhance patient care by bridging communication gaps, providing personalized health insights, and offering emotional and practical support, especially for non-English speakers and less tech-savvy patients.",
+    techs: ["React.js", "Flutter", "PostgreSQL", "Express.js", "Firebase"]
+  },
 ]
 
 export default projects
