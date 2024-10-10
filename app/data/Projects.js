fe4e2e5451741b44ead37ed0e9cf09bf0ef58e55
blin4504 <bl4504@g.rit.edu>
2024-10-09 23:10:05 -0400

    chore: add new project

--- app/data/Projects.js
+++ app/data/Projects.js
@@ -23,6 +23,12 @@ const projects = [
     description: "Project for Google AI Competition. Meddy is an AI-powered medical assistant app designed to enhance patient care by bridging communication gaps, providing personalized health insights, and offering emotional and practical support, especially for non-English speakers and less tech-savvy patients.",
     techs: ["React.js", "Flutter", "PostgreSQL", "Express.js", "Firebase"]
   },
+  {
+    title: "RentBun",
+    link: "https://rentbun.com/",
+    description: "A simple website for users to share and find relets.",
+    techs: ["React.js", "PostgreSQL", "Firebase", "Java", "Spring Boot", "Docker"]
+  },
 ]
 
 export default projects
